6c995c36bbac7f5a097711b1e6df0625d9e238bd
Juliana Amorim <juliana.carol.amorim@gmail.com>
2022-11-15 21:23:08 -0300

    table stuff

--- front/src/app/components/consultas/consultas.component.ts
+++ front/src/app/components/consultas/consultas.component.ts
@@ -1,15 +1,21 @@
+import { ConsultaService } from './../../services/consulta-service.service';
+
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-consultas',
   templateUrl: './consultas.component.html',
-  styleUrls: ['./consultas.component.scss']
+  styleUrls: ['./consultas.component.scss'],
 })
 export class ConsultasComponent implements OnInit {
+  constructor(private service: ConsultaService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
